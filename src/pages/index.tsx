168647b7680fa5f7c56cb4198d5f77a4d81ab9e5
DRS90 <drs.90@hotmail.com>
2022-08-23 01:09:59 -0300

    wip: add pkm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,51 @@
-import Head from 'next/head';
-import { styled } from '@/styles/stitches.config';
+import React, { Suspense, useMemo } from 'react';
 
-const Title = styled('h1', {
-  color: '$hiContrast',
-  variants: {
-    responsive: {
-      true: {
-        fontSize: '$subTitle',
-        '@desktop': {
-          fontSize: '$title'
-        }
-      },
-      false: {
-        fontSize: '$title'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useTexture } from '@react-three/drei';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { useLoader } from '@react-three/fiber';
+import type { Mesh } from 'three';
+
+function Pokemon({ pkmId }: { pkmId: number }) {
+  const obj = useLoader(OBJLoader, `/models/${pkmId}.obj`);
+  const texture = useTexture(`/texture/${pkmId}.png`);
+  const geometry = useMemo(() => {
+    let g;
+    obj.traverse((c) => {
+      if (c.type === 'Mesh') {
+        const _c = c as Mesh;
+        g = _c.geometry;
       }
-    }
+    });
+    return g;
+  }, [obj]);
+  if (geometry) {
+    return (
+      <mesh geometry={geometry} scale={0.1} rotation={[1.5, -1.5, 0]}>
+        <meshPhysicalMaterial map={texture} />
+      </mesh>
+    );
   }
-});
+  return null;
+}
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Nextjs Boilerplate</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Title responsive>Boilerplate</Title>
-      </main>
-    </div>
+    <Canvas
+      camera={{ position: [2, 0, 12.25], fov: 15 }}
+      style={{
+        backgroundColor: '#111a21',
+        width: '100vw',
+        height: '100vh'
+      }}
+    >
+      <ambientLight intensity={1.25} />
+      <ambientLight intensity={0.1} />
+      <directionalLight intensity={0.4} />
+      <Suspense fallback={null}>
+        <Pokemon pkmId={129} />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
   );
 }
