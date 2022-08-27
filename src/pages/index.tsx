9738fbce98975d49fc9a8950d0548a1c5566960b
DRS90 <drs.90@hotmail.com>
2022-08-27 17:17:02 -0300

    wip: loader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+
 import Pokemon3d from '@/components/Pokemon3d';
+
+function Loader() {
+  return null;
+}
 
 export default function Home() {
   return (
@@ -13,10 +18,10 @@ export default function Home() {
         height: '100vh'
       }}
     >
-      <ambientLight intensity={1.25} />
+      <ambientLight intensity={1} />
       <ambientLight intensity={0.1} />
-      <directionalLight intensity={0.4} />
-      <Suspense fallback={null}>
+      <directionalLight intensity={0.5} />
+      <Suspense fallback={<Loader />}>
         <Pokemon3d pokemon="magikarp" />
       </Suspense>
       <OrbitControls />
