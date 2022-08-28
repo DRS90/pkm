e7b5573e4790e9a2db95d3b6c24d1844fcd74642
DRS90 <drs.90@hotmail.com>
2022-08-28 16:06:35 -0300

    feat: create pokemon page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,6 @@
-import { Suspense, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-
-import Pokemon3d from '@/components/Pokemon3d';
-
-function Loader() {
-  return null;
-}
+import Link from 'next/link';
 
 export default function Home() {
-  const [isShiny, setIsShiny] = useState(false);
   return (
     <div
       style={{
@@ -20,29 +11,16 @@ export default function Home() {
         placeItems: 'center'
       }}
     >
-      <div>
-        <label
-          style={{
-            color: 'white',
-            display: 'flex',
-            justifyContent: 'right'
-          }}
-        >
-          <input
-            type="checkbox"
-            checked={isShiny}
-            value={String(isShiny)}
-            onChange={() => setIsShiny(!isShiny)}
-          />
-          Shiny
-        </label>
-        <Canvas style={{ width: '50vw', height: '50vh' }}>
-          <ambientLight intensity={1} />
-          <Suspense fallback={<Loader />} key={String(isShiny)}>
-            <Pokemon3d pokemon="machamp" shiny={Boolean(isShiny)} />
-          </Suspense>
-          <OrbitControls />
-        </Canvas>
+      <div style={{ display: 'grid', gap: 8 }}>
+        <Link href="/charizard">
+          <a>Charizard</a>
+        </Link>
+        <Link href="/magikarp">
+          <a>Magikarp</a>
+        </Link>
+        <Link href="/machamp">
+          <a>Machamp</a>
+        </Link>
       </div>
     </div>
   );
