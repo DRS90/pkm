821578b137e3b2009550d3547a0d93a14705743a
DRS90 <drs.90@hotmail.com>
2022-10-30 12:17:11 -0300

    feat: add ErrorBoundary

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+
+interface Props {
+  children?: ReactNode;
+  Fallback?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public override state: State = {
+    hasError: false
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  public override componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  public override render() {
+    if (this.state.hasError) {
+      return this.props.Fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
