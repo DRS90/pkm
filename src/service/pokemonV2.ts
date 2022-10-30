c8d6907ed28b4abc060c29d891f7aff9b5aae26d
DRS90 <drs.90@hotmail.com>
2022-10-30 11:27:11 -0300

    feat: add getPokemonByName - move types to a new file

--- src/service/pokemonV2.ts
+++ src/service/pokemonV2.ts
@@ -1,28 +1,22 @@
+import type * as T from './@types';
+
 const provider = {
-  basePath: 'https://pokeapi.co/api/v2/',
-  get<T>(url: string): Promise<T> {
+  basePath: 'https://pokeapi.co/api/v2/pokemon',
+  get<T>(url?: string): Promise<T> {
     return fetch(this.basePath + url).then((data) => data.json());
   }
 };
-
-export interface SimplePokemonResponse {
-  name: string;
-  url: string;
-}
-
-export interface PokemonResponse {
-  previous: string | null;
-  next: string | null;
-  count: number;
-  results: SimplePokemonResponse[];
-}
 
 export async function getAllPokemon({
   limit
 }: {
   limit?: string;
   offset?: string;
-} = {}): Promise<PokemonResponse['results']> {
-  const data = await provider.get<PokemonResponse>(`/pokemon?limit=${limit}`);
+} = {}): Promise<T.AllPokemonResponse['results']> {
+  const data = await provider.get<T.AllPokemonResponse>(`?limit=${limit}`);
   return data.results;
+}
+
+export async function getPokemonByName(pokemonName: string) {
+  return await provider.get<T.PokemonResponse>(`/${pokemonName}`);
 }
